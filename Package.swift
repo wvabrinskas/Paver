5165c8394fa89220079072b84ea8460144398867
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-08-24 17:03:09 -0400

    added code"

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Paver",
+    platforms: [ .iOS(.v13),
+                 .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
--- Sources/Paver/Paver.swift
+++
@@ -1,6 +0,0 @@
-public struct Paver {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
